a27a4f285fe1a3707f94745fcab568a21a673e88
ConwayCJ <conwaycj@plu.edu>
2023-05-15 13:27:17 -0700

    add week 8 practice and assignment

--- week8/week8assignment/src/main/resources/project_sql.sql
+++ week8/week8assignment/src/main/resources/project_sql.sql
@@ -1,8 +1,45 @@
+DROP TABLE IF EXISTS project_category;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS step;
+DROP TABLE IF EXISTS material;
+DROP TABLE IF EXISTS project;
+
 create table project (
-    project_id int not null primary,
+    project_id int not null auto_increment,
     project_name varchar(128) not null,
-    estimated_hours decimal(7,2) not null,
-    actual_hours decimal(7,2) not null,
+    estimated_hours decimal(7,2),
+    actual_hours decimal(7,2),
     difficulty int,
-    notes text
+    notes text,
+    PRIMARY KEY (project_id)
+);
+
+create table material (
+    material_id int not null auto_increment,
+    project_id int not null,
+    material_name varchar(128) not null,
+    num_required int,
+    cost decimal(7,2),
+    PRIMARY KEY (material_id),
+    FOREIGN KEY (project_id) references project (projec_id)
+);
+
+create table step (
+	step_id int not null auto_increment,
+    project_id int not null references project(project_id),
+    step_text text,
+    step_order int,
+    PRIMARY KEY step_id,
+    FOREIGN KEY (project_id) references project (project_id)
+);
+
+create table category (
+	category_id int not null primary key auto_increment,
+    category_name varchar(128) not null
+);
+
+create table project_category (
+	project_id int references project(project_id),
+    category_id int references category(category_id),
+    unique key (project_id, category_id)
 )
